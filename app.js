b591d2b8150a19f722c21254466a67ef56bc2988
tempDev001 <tempDev001@bukteez.com.ng>
2021-04-06 18:23:42 +0100

    improved the app script

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const h1 = document.querySelector('.h1');
+const myL = document.querySelector('li:nth-of-type(3)');
 
 h1.addEventListener('click', event => {
 	console.log(event.target.textContent);
 	h1.textContent = 'I don switch am';
+	myL.style.cssText = 'background-color: yellow; padding: 1rem; font-weight: bold; border-radius: 2rem';
+	
+	myL.addEventListener('click', printSom, {
+		once: true
+	});
 });
+
+const printSom = event => {
+	console.log(event.target.textContent);
+}
